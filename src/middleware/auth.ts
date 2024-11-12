d969371a33a9272b4e5f16361abd17f1296270ad
Volodymyr <zomkovolodymyr@gmail.com>
2024-11-12 14:21:06 +0100

    Manage restaurant page

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -16,7 +16,7 @@ declare global {
 
 export const jwtCheck = auth({
    audience: process.env.AUTH0_AUDIENCE,
-   issuerBaseURL: process.env.AUTH0_BASE_URL,
+   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
    tokenSigningAlg: 'RS256'
 });
 
