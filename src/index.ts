f3263d6413576064ab80a0a133ddc2edb4603706
Volodymyr <zomkovolodymyr@gmail.com>
2024-11-19 22:20:42 +0100

    added checkout flow

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import myUserRoute from "./routes/MyUserRoute"
 import { v2 as cloudinary } from "cloudinary"
 import myRestaurantRoute from "./routes/MyRestaurantRoute"
 import restaurantRoute from "./routes/RestaurantRoute"
+import orderRoute from "./routes/OrderRoute"
 
 mongoose
    .connect(process.env.MONGO_CONNECTION_STRING as string)
@@ -18,17 +19,22 @@ cloudinary.config({
 });
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
 
-app.get("/health", async (req: Request, res: Response) => {
-   res.send({ message: "Health OK!" })
-})
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
 
-app.use("/api/my/user", myUserRoute)
+app.use(express.json());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
+
+app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
-app.use("/api/restaurant", restaurantRoute)
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(7000, () => {
-   console.log("Server started on localhost:7000")
+  console.log("server started on localhost:7000");
 });
